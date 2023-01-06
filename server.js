7ff3ab46045d54d57338cb8f1b219e0618565796
jakhongirov <johongirovdiyorbek08@gmail.com>
2023-01-06 09:48:37 +0500

    Add

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
-const requestIP = require('request-ip');
-const bcryptjs = require('bcryptjs');
-const bodyParser = require('body-parser');
+const path = require('path')
 const app = express();
 const { PORT } = require("./src/config");
 const router = require("./src/modules");
@@ -11,7 +9,7 @@ const router = require("./src/modules");
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
-app.use('/public', express.static(path.resolve(__dirname, '..','public')))
+app.use('/public', express.static(path.resolve(__dirname, "..",'public')))
 app.use("/api/v1", router);
 
 app.listen(PORT, console.log(PORT));
