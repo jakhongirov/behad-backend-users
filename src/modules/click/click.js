9b534e908ec27785f760d867adfda79a41347efd
jakhongirov <johongirovdiyorbek08@gmail.com>
2023-01-09 18:07:00 +0500

    add

--- src/modules/click/click.js
+++ src/modules/click/click.js
@@ -10,16 +10,21 @@ module.exports = {
             merchant_prepare_id: 5,
             merchant_trans_id: merchant_trans_id,
             click_trans_id: click_trans_id,
-            merchant_confirm_id: null,
             error: error,
             error_note: error_note
         })
     },
 
     POST_2: async (req, res) => {
-        const body = req.body
-        console.log(body);
-        console.log("clickComplete");
-        res.send("ok")
+        const  {click_trans_id, merchant_trans_id, error, error_note } = req.body
+
+        res.json({
+            merchant_prepare_id: 5,
+            merchant_trans_id: merchant_trans_id,
+            click_trans_id: click_trans_id,
+            merchant_confirm_id: null,
+            error: error,
+            error_note: error_note
+        })
     }
 }
