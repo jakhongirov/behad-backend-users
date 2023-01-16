b36dfde44fe651e5eae19ab1b9847323a9cdcac2
jakhongirov <johongirovdiyorbek08@gmail.com>
2023-01-16 18:10:28 +0500

    add

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const router = require("./src/modules");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: '*',
-    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
 }));
 app.use(express.json());
 app.use('/public', express.static(path.resolve(__dirname, "..", 'public')))
