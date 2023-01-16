7740316a76dca0af90dadc4ada10f312a5721295
jakhongirov <johongirovdiyorbek08@gmail.com>
2023-01-16 18:50:07 +0500

    add

--- server.js
+++ server.js
@@ -9,18 +9,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     "origin": "*",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
 }));
 app.use(express.json());
 app.use('/public', express.static(path.resolve(__dirname, "..", 'public')))
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
 app.use("/api/v1", router);
 
 app.listen(PORT, console.log(PORT));
