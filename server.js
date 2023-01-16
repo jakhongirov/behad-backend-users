09ed00d3a5a71e2002f024425e85ceb5d632e4b7
jakhongirov <johongirovdiyorbek08@gmail.com>
2023-01-16 18:25:32 +0500

    add

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(express.json());
 app.use('/public', express.static(path.resolve(__dirname, "..", 'public')))
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
