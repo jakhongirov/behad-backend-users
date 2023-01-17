5be15f8848156d23711ff8657c8574c401ccdfad
jakhongirov <johongirovdiyorbek08@gmail.com>
2023-01-17 13:27:43 +0500

    add

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ const app = express();
 const { PORT } = require("./src/config");
 const router = require("./src/modules");
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors())
 app.use(express.json());
 app.use('/public', express.static(path.resolve(__dirname, "..", 'public')))
 app.use("/api/v1", router);
 
-app.listen(PORT, console.log(PORT));
+app.listen(PORT, console.log(cors()));
