1c4aa6095c9ec364c0e10effd8abf6213c4ed957
jakhongirov <johongirovdiyorbek08@gmail.com>
2023-01-17 12:30:50 +0500

    add

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ const { PORT } = require("./src/config");
 const router = require("./src/modules");
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/public', express.static(path.resolve(__dirname, "..", 'public')))
 app.use("/api/v1", router);
