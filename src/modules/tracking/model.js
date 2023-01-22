a8672c40a419384732189a5dd09b55d5d4be2218
jakhongirov <johongirovdiyorbek08@gmail.com>
2023-01-22 21:16:09 +0500

    add

--- src/modules/tracking/model.js
+++ src/modules/tracking/model.js
@@ -1,16 +1,22 @@
 const { fetchALL } = require("../../lib/postgres");
 
 const ALL_TRACKING_USERS = `
-    SELECT
-        *,  to_char(new_tracking_user_create_date at time zone 'Asia/Tashkent', 'HH24:MM/MM.DD.YYYY')
-    FROM
-        tracking_users
+    select 
+    *, to_char(new_tracking_user_create_date at time zone 'Asia/Tashkent', 'HH24:MM/MM.DD.YYYY')
+    from
+        tracking_users a
+    inner join
+        users b
+    on a.user_id = b.user_id
+    inner join
+        apps c
+    on a.app_key = c.app_key
     ORDER BY
-        tracking_user_id DESC;
+        a.tracking_user_id DESC;
 `;
 
 const getTrackingUsers = () => fetchALL(ALL_TRACKING_USERS)
 
 module.exports = {
     getTrackingUsers
-}
+}   
